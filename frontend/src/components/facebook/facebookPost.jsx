6959573e7a10a996911944b17762a9a576107e66
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-08-01 18:24:11 +0530

    Correct the Components

---
+++ frontend/src/components/facebook/facebookPost.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen flex flex-col bg-black text-white">
+      <main className="flex flex-1 items-center justify-center mt-18">
+        <h1 className="text-4xl">Welcome to the Facebook Page</h1>
+      </main>
+    </div>
+  );
+}
