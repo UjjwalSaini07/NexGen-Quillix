d180775a4fc7db394a9b3e22100682b52064d34f
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-08-27 16:09:14 +0530

    Add footer component and update fonts in layout
    Introduced a new Footer component for the homepage, featuring social links and navigation columns. Updated layout.js to import additional Google fonts (Ancizar Serif, Orbitron, Playfair Display SC) for enhanced typography. Integrated the Footer into the main page layout.

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,6 +3,7 @@
 import React from "react";
 import Header from "../components/common/Header";
 import "./index.css";
+import FooterPage from "@/components/home/footer";
 
 export default function HomePage() {
   return (
@@ -11,6 +12,9 @@ export default function HomePage() {
       <main className="flex flex-1 items-center justify-center mt-18">
         <h1 className="text-4xl">Welcome to the Homepage</h1>
       </main>
+      <footer>
+        <FooterPage/>
+      </footer>
     </div>
   );
 }
