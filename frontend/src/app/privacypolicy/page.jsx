6f8696835676f6f893da1f26171fc8753da081eb
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2025-08-27 17:53:29 +0530

    Add Privacy Policy page and component
    Introduced a new Privacy Policy page at frontend/src/app/privacypolicy/page.jsx and a reusable PrivacyPolicy component in frontend/src/components/legals/privacypolicy.jsx. Integrated framer-motion for animations and updated package.json to include framer-motion as a dependency.

---
+++ frontend/src/app/privacypolicy/page.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { useEffect } from "react";
+import "../index.css";
+import { toast } from "react-toastify";
+import Header from "../../components/common/Header";
+import PrivacyPolicy from "../../components/legals/privacypolicy";
+
+export default function PrivacyPolicyPage() {
+  useEffect(() => {
+    toast.info("Welcome to NexGen-Quillix's Privacy Policy page! 📝");
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, []);
+
+  return (
+    <div className="min-h-screen flex flex-col bg-black text-white overflow-hidden">
+      <Header />
+      <main className="items-center justify-center mt-18">
+        <PrivacyPolicy />
+      </main>
+    </div>
+  );
+}
